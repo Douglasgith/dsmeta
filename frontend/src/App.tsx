3a88de50ee668bc4cd168c190effc613c1e73009
Douglas Machado <dgsekofoz@gmail.com>
2022-07-11 22:37:12 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import NotificationButton from "./components/NotificationButton";
+
+import Header from "./components/NotificationButton/header";
+import SalesCard from "./components/NotificationButton/SalesCard";
 
 function App() {
   return (
     <>
-      <h1>ola mundo!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }

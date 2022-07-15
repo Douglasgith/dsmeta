f5274514ca0bd7e1fbf83d3d4be1ca0fa86d07e3
Douglas Machado <dgsekofoz@gmail.com>
2022-07-15 20:32:59 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/NotificationButton/header";
 import SalesCard from "./components/NotificationButton/SalesCard";
 
 function App() {
   return (
     <>
+    <ToastContainer/>
       <Header />
       <main>
         <section id="sales">
